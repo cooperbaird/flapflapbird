236cc23b9f594d06ef5085e3a07b8dfed94c2a32
cooperbaird <cooperbaird@coopers-mbp.ad.wlu.edu>
2018-03-04 14:45:22 -0500

    Fixed format

--- FlapFlapBird/src/core/ScoreOperations.java
+++ FlapFlapBird/src/core/ScoreOperations.java
@@ -10,39 +10,39 @@ import java.util.prefs.Preferences;
 public class ScoreOperations {
 	private int score, highScore;
 	private Preferences prefs;
-	
+
 	public ScoreOperations() {
-		score = 0;	
+		score = 0;
 		prefs = Preferences.userNodeForPackage(ScoreOperations.class);
 		highScore = prefs.getInt("flapflapbird_highscore", 0);
 	}
-    
-    public void editHighScore() {
-        if(score > highScore) {
-        		highScore = score;
-        		prefs.putInt("flapflapbird_highscore", highScore);
-        }
-    }
-    
-    public void increaseScore() {
-    		score++;
-    }
-    
-    public void resetScore() {
-    		score = 0;
-    }
-    
-    /**
-     * @return the integer value for the player's score
-     */
-    public int getScore() {
-    		return score;
-    }
-    
-    /**
-     * @return the integer value for the player's high score
-     */
-    public int getHighScore() {
-    		return highScore;
-    }
+
+	public void editHighScore() {
+		if (score > highScore) {
+			highScore = score;
+			prefs.putInt("flapflapbird_highscore", highScore);
+		}
+	}
+
+	public void increaseScore() {
+		score++;
+	}
+
+	public void resetScore() {
+		score = 0;
+	}
+
+	/**
+	 * @return the integer value for the player's score
+	 */
+	public int getScore() {
+		return score;
+	}
+
+	/**
+	 * @return the integer value for the player's high score
+	 */
+	public int getHighScore() {
+		return highScore;
+	}
 }
